84bdedceff98c2b8d9fac871c2cfbbf73a966cba
jalshah-30 <jalshahpcps9thb@gmail.com>
2026-07-29 23:13:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -234,7 +234,7 @@ with st.sidebar:
     )
 
     vote_count = st.number_input(
-        "Interesed(BookMyShow)",
+        "Interested(BookMyShow)",
         min_value=0,
         max_value=int(df["vote_count"].max()),
         value=1000,
